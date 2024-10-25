ae7b5cea4a07ebe4a84b83bf2aa43f2b3a229e13
matheushmg <matheus.henrique@shiftinc.com.br>
2024-10-25 15:55:41 -0300

    feat: configuration and code enhancement

---
+++ src/user/user.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ICreateUserDto } from './dtos/create.dto';
+import { UserService } from './user.service';
+import { UserEntity } from './interfaces/user.entity';
+
+@Controller('user')
+export class UserController {
+
+    constructor(private readonly userService: UserService) {
+
+    }
+
+    @Get()
+    async getAllUser(): Promise<UserEntity[]> {
+        return this.userService.getAllUser();
+    }
+
+    @Post()
+    async create(
+        @Body() create: ICreateUserDto
+    ): Promise<UserEntity> {
+        return this.userService.create(create);
+    }
+}
